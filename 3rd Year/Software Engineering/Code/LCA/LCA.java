ac13d1e86c218a54bf31ba08103a8b90563ee8d5
Tonga6 <60393889+Tonga6@users.noreply.github.com>
2020-11-11 14:15:11 +0000

    Began work on new tests for LCA-DAG

--- 3rd Year/Software Engineering/Code/LCA/LCA.java
+++ 3rd Year/Software Engineering/Code/LCA/LCA.java
@@ -1,12 +1,8 @@
-//Test lines added for git testing
-
-// Finds for Lowest Common Ancestor in a Binary Tree 
-// A O(n) solution to find LCA of two given values n1 and n2 
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List; 
   
-// A Binary Tree node 
+// A DAG node
 class Node { 
     int data; 
     ArrayList<Node> children = new ArrayList<Node>();
@@ -21,50 +17,50 @@ class Node {
   
 public class LCA  
 {   
-    private ArrayList<Node> nodes = new ArrayList<Node>(); 
-    
-    public static void main(String[] args) 
-    { 
-        LCA graph = new LCA();
-        for(int i = 0;i < 7; i++){ //create graph with 6 nodes
-          graph.nodes.add(i,new Node(i+1));
-           
-        }
-        graph.nodes.get(0).children.add(0,graph.nodes.get(1));
-        graph.nodes.get(0).children.add(1,graph.nodes.get(2));
-        graph.nodes.get(0).children.add(2,graph.nodes.get(4));
-        graph.nodes.get(0).children.add(3,graph.nodes.get(5));
-
-        graph.nodes.get(1).parents.add(0,graph.nodes.get(0));
-        graph.nodes.get(1).children.add(0,graph.nodes.get(3));
-        graph.nodes.get(1).children.add(1,graph.nodes.get(6));
-
-        graph.nodes.get(2).parents.add(0,graph.nodes.get(0));
-        graph.nodes.get(2).children.add(0,graph.nodes.get(3));
-
-        graph.nodes.get(3).parents.add(0,graph.nodes.get(1));
-        graph.nodes.get(3).parents.add(1,graph.nodes.get(2));
-        graph.nodes.get(3).parents.add(2,graph.nodes.get(4));
-        graph.nodes.get(3).children.add(0,graph.nodes.get(6));
-
-        graph.nodes.get(4).parents.add(0,graph.nodes.get(0));
-        graph.nodes.get(4).children.add(0,graph.nodes.get(4));
-        graph.nodes.get(4).children.add(1,graph.nodes.get(6));
-
-        graph.nodes.get(5).parents.add(0,graph.nodes.get(0));
-
-        graph.nodes.get(6).parents.add(0,graph.nodes.get(1));
-        graph.nodes.get(6).parents.add(1,graph.nodes.get(3));
-        graph.nodes.get(6).parents.add(2,graph.nodes.get(4));
-    
-        ArrayList<ArrayList<Node>> allParentsA = new ArrayList<ArrayList<Node>>();
-        ArrayList<ArrayList<Node>> allParentsB = new ArrayList<ArrayList<Node>>();
-        getAllParents(graph, allParentsA, graph.nodes.get(6), 0);
-        getAllParents(graph, allParentsB, graph.nodes.get(3), 0);
-        ArrayList<ArrayList<Node>> arrLCA = new ArrayList<ArrayList<Node>>();
-        getLCA(arrLCA, allParentsA, allParentsB);
-        System.out.println("End");
-    }  
+    ArrayList<Node> nodes = new ArrayList<Node>(); 
+//    
+//     public static void main(String[] args) 
+//     { 
+//    	 LCA graph = new LCA();
+//         for(int i = 0;i < 7; i++){ //create graph with 6 nodes
+//           graph.nodes.add(i,new Node(i+1));
+//            
+//         }
+//         graph.nodes.get(0).children.add(0,graph.nodes.get(1));
+//         graph.nodes.get(0).children.add(1,graph.nodes.get(2));
+//         graph.nodes.get(0).children.add(2,graph.nodes.get(4));
+//         graph.nodes.get(0).children.add(3,graph.nodes.get(5));
+//
+//         graph.nodes.get(1).parents.add(0,graph.nodes.get(0));
+//         graph.nodes.get(1).children.add(0,graph.nodes.get(3));
+//         graph.nodes.get(1).children.add(1,graph.nodes.get(6));
+//
+//         graph.nodes.get(2).parents.add(0,graph.nodes.get(0));
+//         graph.nodes.get(2).children.add(0,graph.nodes.get(3));
+//
+//         graph.nodes.get(3).parents.add(0,graph.nodes.get(1));
+//         graph.nodes.get(3).parents.add(1,graph.nodes.get(2));
+//         graph.nodes.get(3).parents.add(2,graph.nodes.get(4));
+//         graph.nodes.get(3).children.add(0,graph.nodes.get(6));
+//
+//         graph.nodes.get(4).parents.add(0,graph.nodes.get(0));
+//         graph.nodes.get(4).children.add(0,graph.nodes.get(4));
+//         graph.nodes.get(4).children.add(1,graph.nodes.get(6));
+//
+//         graph.nodes.get(5).parents.add(0,graph.nodes.get(0));
+//
+//         graph.nodes.get(6).parents.add(0,graph.nodes.get(1));
+//         graph.nodes.get(6).parents.add(1,graph.nodes.get(3));
+//         graph.nodes.get(6).parents.add(2,graph.nodes.get(4));
+//     
+//         ArrayList<ArrayList<Node>> allParentsA = new ArrayList<ArrayList<Node>>();
+//         ArrayList<ArrayList<Node>> allParentsB = new ArrayList<ArrayList<Node>>();
+//         getAllParents(graph, allParentsA, graph.nodes.get(6), 0);
+//         getAllParents(graph, allParentsB, graph.nodes.get(3), 0);
+//         ArrayList<ArrayList<Node>> arrLCA = new ArrayList<ArrayList<Node>>();
+//         getLCA(arrLCA, allParentsA, allParentsB);
+//         System.out.println("End");
+   // }  
   
     static void getAllParents (LCA graph, ArrayList<ArrayList<Node>> allParents, Node node, int depth){
          
@@ -99,63 +95,7 @@ public class LCA
                 if (!allParentsA.get(i).isEmpty()/* && Math.max(i,j) maxLCADepth */)
                     arrayLCA.add(0,allParentsA.get(i));
                     minLCADepth = Math.max(i,j);    //new min LCA to beat or join
-            }
-            
-        }
-         
+            }            
+        }         
     }
-
-
-
-    // // Finds the path from root node to given root of the tree. 
-    // int findLCA(int n1, int n2) { 
-    //     pathA.clear(); 
-    //     pathB.clear(); 
-    //     return findLCAInternal(root, n1, n2); 
-    // } 
-  
-    // private int findLCAInternal(Node root, int n1, int n2) { 
-  
-    //     if (!findPath(root, n1, pathA) || !findPath(root, n2, pathB)) { 
-    //         System.out.println((pathA.size() > 0) ? "n1 is present" : "n1 is missing"); 
-    //         System.out.println((pathB.size() > 0) ? "n2 is present" : "n2 is missing"); 
-    //         return -1; 
-    //     } 
-  
-    //     int i; 
-    //     for (i = 0; i < pathA.size() && i < pathB.size(); i++) { 
-    //         if (!pathA.get(i).equals(pathB.get(i))) 
-    //             break; 
-    //     }   
-    //     return pathA.get(i-1); 
-    // } 
-      
-    // // Finds the path from root node to given root of the tree, Stores the 
-    // // path in a vector path[], returns true if path exists otherwise false 
-    // private boolean findPath(Node root, int n, List<Integer> path) 
-    // { 
-    //     // base case 
-    //     if (root == null) { 
-    //         return false; 
-    //     } 
-          
-    //     // Store this node . The node will be removed if 
-    //     // not in path from root to n. 
-    //     path.add(root.data); 
-  
-    //     if (root.data == n) { 
-    //         return true; 
-    //     }   
-    //     if (root.left != null && findPath(root.left, n, path)) { 
-    //         return true; 
-    //     }   
-    //     if (root.right != null && findPath(root.right, n, path)) { 
-    //         return true; 
-    //     } 
-  
-    //     // If not present in subtree rooted with root, remove root from 
-    //     // path[] and return false 
-    //     path.remove(path.size()-1);   
-    //     return false; 
-    // }     
 } 
